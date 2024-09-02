30a9b9bd903cccd6b5432b75a74f9c3409187090
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-01 23:04:09 -0400

    Recipe Options Type

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,22 @@
-import { ObjectId } from "mongodb"
+import { ObjectId } from "mongodb";
 
 export type Recipe = {
-    _id: ObjectId;
-    recipe_title: string;
-    recipe_content: string;
-    recipe_image: string;
-}
+  _id: ObjectId;
+  recipe_title: string;
+  recipe_content: string;
+  recipe_image: string;
+};
+
+export type RecipeOptions = {
+  dietaryPreferences: string;
+  cuisineType: string;
+  mealType: string;
+  cookingTime: string;
+  servingSize: string;
+  difficultyLevel: string;
+  flavorProfile: string;
+  cookingMethod: string;
+  nutritionalPreference: string;
+  occasion: string;
+  allergenFilters: string[];
+};
